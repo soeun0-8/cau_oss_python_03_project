03163a857fbbdcd32ecd316b775eec04fa2a442b
Soeun Park <eysl018@cau.ac.kr>
2023-06-16 23:13:55 +0900

    version #2: Modified menu_selector.py & parking_spot_manager.py
    menu_selctor.py: implement Menu [1] print, [4] exit
    
    parking_spot_manager.py: implement class parking_spot, functions about reading data and printing

--- menu_selector.py
+++ menu_selector.py
@@ -1,4 +1,45 @@
+"""무료 주차 공간 데이터 출력, 분석, 관리에 대한 메뉴 제공 모듈
+
+해당 모듈은 함수 start_process를 통해
+메뉴를 제공하며 간단한 숫자와 문자 입력만으로
+사용자와 상호작용함으로써
+무료 주차 공간 데이터를 출력하고,
+입력받은 키워드에 따라 데이터를 추려내고
+정렬할 수 있도록 하는 기능을 수행합니다.
+
+Version #2: 메뉴 [1] print, [4] exit 구현
+"""
+
+import file_manager
+import parking_spot_manager
+# from parking_spot_manager import *: 메소드 사용시 모듈명 붙일 필요 x
+# (어떤 모듈에 구현된 메소드인지 확인 위해 해당 파일에서는 사용하지 않음)
+
 def start_process(path):
+    """무료 주차 공간 데이터에 관한 메뉴 제공
+
+    숫자 입력을 통해 접근 가능한
+    무료 주차 공간 데이터를 활용하는 메뉴 3가지와
+    종료를 위한 메뉴 1가지를 제공합니다.
+    첫번째 메뉴 print는 주차 공간 데이터를
+    출력하도록 하며,
+    두번째 메뉴 filter는 주차 공간 데이터를
+    입력한 키워드에 따라 추려내고,
+    세번째 메뉴 sort는 키워드를 기준으로
+    데이터를 정렬합니다.
+    네번째 메뉴 exit은 반복되는 메뉴를
+    종료하는 기능을 수행합니다.
+    exit을 선택하지 않는 한 특정 메뉴를 통한
+    기능 수행 후에도 메뉴를 반복해서 제시합니다.
+
+    Args:
+        path (str): 주차 공간 데이터가 담긴 파일의 경로
+
+    """
+
+    str_list = file_manager.read_file(path)
+    spots = parking_spot_manager.str_list_to_class_list(str_list)
+
     while True:
         print("---menu---")
         print("[1] print")
@@ -7,8 +48,8 @@ def start_process(path):
         print("[4] exit")
         select = int(input('type:'))
         if select == 1:
-            print("not implemented yet")
-            # fill this block
+            parking_spot_manager.print_spots(spots)
+
         elif select == 2:
             print("---filter by---")
             print("[1] name")
@@ -52,7 +93,8 @@ def start_process(path):
                 # fill this block
             else: print("invalid input")
         elif select == 4:
-            print("not implemented yet")
-            # fill this block
+            print("Exit")
+            break
+
         else:
             print("invalid input")
--- parking_spot_manager.py
+++ parking_spot_manager.py
@@ -1,19 +1,116 @@
+"""무료 주차 공간에 관련된 클래스 및 함수 구현 모듈
+
+해당 모듈은 무료 주차 공간의 데이터를
+저장, 분석, 관리하고 출력합니다.
+클래스 parking_spot을 활용하여 주차 공간 정보를
+저장하고 반환하며, str_list_to_class_list 함수를 통해
+주차 공간에 대한 문자열 리스트를
+클래스 parking_spot 객체 리스트로 변환 및 반환,
+print_spots 함수를 통해 주차 공간 정보를
+출력하는 기능을 수행합니다.
+
+Version #2: 클래스 parking_spot과
+함수 str_list_to_class_list, print_spots 구현
+"""
+
 class parking_spot:
-    # you have to implement 'constructor(생성자)' and 'get' method
+    """주차 공간의 정보를 저장하는 클래스
+
+    주차 공간의 정보 6가지를 생성자를 통해 객체 정보로 저장합니다.
+    해당 정보를 알아보기 쉽게 string 타입으로 정리한 메소드 __str__와,
+    객체 변수를 반환하는 메소드 get을 제공합니다.
+    """
+
+    def __init__(self, name, city, district, ptype, longitude, latitude):
+        """주차 공간의 정보를 받는 생성자
+
+        주차 공간의 정보인 자원명, 시도, 시군구,
+        주차장유형, 경도, 위도를 받습니다.
+
+        Args:
+            name (str): 자원명
+            city (str): 시도
+            district (str): 시군구
+            ptype (str): 주차장 유형
+            longitude (float): 경도
+            latitude (float): 위도
+        """
+
+        self.__item = {'name': name, 'city': city, 'district': district,
+                       'ptype': ptype, 'longitude': longitude, 'latitude': latitude}
+
     def __str__(self):
+        """객체가 저장한 주차 공간 정보 반환
+
+        주차 공간의 정보 6가지를
+        가독성 높도록 포맷팅한 문자열 s로 반환합니다.
+
+        Returns:
+            str: '[자원명(주차장 유형)] 시도 시군구(lat:위도, long:경도)' 형태
+        """
+
         item = self.__item
         s  = f"[{item['name']}({item['ptype']})] "
         s += f"{item['city']} {item['district']}"
         s += f"(lat:{item['latitude']}, long:{item['longitude']})"
         return s
 
+    def get(self, keyword = 'name'): # 기본인수 name 설정
+        """객체가 저장한 주차 공간 정보 6가지 중 요청 정보 반환
+
+        주차 공간의 정보 6가지 중
+        인수를 통해 입력한 종류의 정보를 반환합니다.
+
+        Args:
+            keyword (str): 주차 공간 정보의 한 종류, 기본인수는 'name'
+
+        Returns:
+            str or float: 자원명, 시도, 시군구, 주차장유형, 경도, 위도 중 입력된 키워드에 해당하는 종류의 정보
+        """
+
+        return self.__item[keyword]
+
+
+# for 문으로 str_list 탐색 -> split 함수 통해 쉼표로 구분된 데이터 분리 -> 분리한 데이터 spot 생성자에 넣어 인스턴스 spot 생성
+# -> spot을 spots 객체 리스트에 append
+def str_list_to_class_list(str_list):
+    """주차 공간 정보가 담긴 문자열 리스트를 parking_spot 클래스 객체 리스트로 변환하여 반환
+
+    Args:
+        str_list (list): 주차 공간 정보가 담긴 문자열 리스트
+
+    Returns:
+        list: 주차 공간 정보를 저장한 객체가 담긴 객체 리스트
+    """
+
+    spots = []
+
+    for str in str_list:
+        spot_data = str.split(',')
+        spot = parking_spot(spot_data[1], spot_data[2], spot_data[3],
+                            spot_data[4], float(spot_data[5]), float(spot_data[6]))
+        spots.append(spot)
+
+    return spots
+
+def print_spots(spots):
+    """주차 공간 정보가 담긴 객체 리스트 출력
+
+    Args:
+        spots (list): 주차 공간 정보가 담긴 객체 리스트
+    """
+
+    print("---print elements([", len(spots), "])---", sep='')
+
+    for spot in spots:
+        print(spot.__str__())
 
 # 각 단계별로 테스트 (테스트할때 주석해제 후 사용)
 if __name__ == '__main__':
     print("Testing the module...")
     # version#2
     # import file_manager
-    # str_list = file_manager.from_file("./input/free_parking_spot_seoul.csv")
+    # str_list = file_manager.read_file("./input/free_parking_spot_seoul.csv")
     # spots = str_list_to_class_list(str_list)
     # print_spots(spots)
 
